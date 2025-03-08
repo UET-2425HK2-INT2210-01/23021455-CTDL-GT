cde12e9fc3c7d50e3f15afc38be06c6b1b19a51d
Nguyễn Thị Hạnh Vi <23021455@vnu.edu.vn>
2025-03-08 11:04:00 +0700

    Delete 3.1.cpp

--- 3.1.cpp
+++
@@ -1,8 +0,0 @@
-// Danh sach cac ham duoc sap xep thu tu tang dan cua Big O notation:
-O(1) -> 2^10
-O(logn) -> 2logn
-O(n) -> 3n + 100logn, 4n
-O(n logn) -> nlogn, 4nlogn + 2n
-O(n^2) -> n^2 + 10n
-O(n^3) -> n^3
-O(2^n) -> 2^n 
