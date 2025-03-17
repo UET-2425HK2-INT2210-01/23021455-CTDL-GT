dfe89b1f1d3c499540b24ae555e31667dec26838
Nguyễn Thị Hạnh Vi <23021455@vnu.edu.vn>
2025-03-17 22:36:46 +0700

    Add files via upload

---
+++ Lect4_Assignments/4.2.cpp
@@ -0,0 +1,28 @@
+Chaining						
+Key	Values		                      hash function h(i) = 2i + 5 mod 11			
+0	empty		                            i	2i + 5	mod 11	
+1	20		                                12	  29	7	
+2	empty	                               	44	  93	5	
+3	empty		                            13	  31	9	
+4	16 -> 5		                            88	 181	5	6
+5	44 -> 88 -> 11		                    23	  51	7	8
+6	94 -> 39		                        94	 193	6	
+7	12 -> 23		                        11	  27	5	10
+8	empty		                            39	  83	6	0
+9	13		                                20	  45	1	
+10	empty		                            16	  37	4	2
+			                                5	  15	4	3
+Linear probing						
+Key	Values					
+0	39					
+1	20					
+2	16					
+3	4					
+4	94					
+5	44					
+6	88					
+7	12					
+8	23					
+9	13					
+10	11					
+
