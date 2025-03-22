e5ee0adfe976cdf0bb38666be9fc9914f0c18b93
Nguyễn Thị Hạnh Vi <23021455@vnu.edu.vn>
2025-03-22 12:19:13 +0700

    Update 4.2.cpp

--- Lect4_Assignments/4.2.cpp
+++ Lect4_Assignments/4.2.cpp
@@ -1,17 +1,17 @@
 Chaining						
 Key	Values		                      hash function h(i) = 2i + 5 mod 11			
 0	empty		                            i	2i + 5	mod 11	
-1	20		                                12	  29	7	
-2	empty	                               	44	  93	5	
+1	20		                            12	  29	7	
+2	empty	                               	    44	  93	5	
 3	empty		                            13	  31	9	
 4	16 -> 5		                            88	 181	5	6
 5	44 -> 88 -> 11		                    23	  51	7	8
-6	94 -> 39		                        94	 193	6	
-7	12 -> 23		                        11	  27	5	10
+6	94 -> 39		                    94	 193	6	
+7	12 -> 23		                    11	  27	5	10
 8	empty		                            39	  83	6	0
-9	13		                                20	  45	1	
+9	13		                            20	  45	1	
 10	empty		                            16	  37	4	2
-			                                5	  15	4	3
+			                             5	  15	4	3
 Linear probing						
 Key	Values					
 0	39					
