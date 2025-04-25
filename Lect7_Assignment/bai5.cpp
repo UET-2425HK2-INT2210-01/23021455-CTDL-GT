8221aee6acece8ea5d933ff90326c98a53ffc58c
Nguyễn Thị Hạnh Vi <23021455@vnu.edu.vn>
2025-04-25 23:28:04 +0700

    Update bai5.cpp

--- Lect7_Assignment/bai5.cpp
+++ Lect7_Assignment/bai5.cpp
@@ -1,20 +1,24 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
 using namespace std;
 
-int main(){
-	int n;
-	cin >> n;
-	vector<int> nums;
-	for (int i = 1; i <= n; ++i){
-		nums.push_back(i);
-	}
-	sort(nums.begin(), nums.end());
-	
-	do{
-		for (int i = 0; i < n; ++i){
-			cout << nums[i];
-		}
-		cout << endl;
-	}while (next_permutation(nums.begin(), nums.end()));
-	return 0;
+int main() {
+    int n;
+    cin >> n; 
+
+    vector<int> nums; // Vector để chứa các phần tử từ 1 đến n
+    for (int i = 1; i <= n; ++i) {
+        nums.push_back(i); // Thêm các số từ 1 đến n vào vector
+    }
+
+    sort(nums.begin(), nums.end()); // Sắp xếp vector nums để chuẩn bị cho việc sinh hoán vị
+
+    // Sinh và in tất cả các hoán vị của dãy số
+    do {
+        for (int i = 0; i < n; ++i) {
+            cout << nums[i]; // In ra hoán vị hiện tại
+        }
+        cout << endl; 
+    } while (next_permutation(nums.begin(), nums.end())); // Tiến tới hoán vị tiếp theo
+
+    return 0; 
 }
