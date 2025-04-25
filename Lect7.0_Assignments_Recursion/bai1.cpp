0a48595f35ea6ace0bc4787e545746f1e1212451
Nguyễn Thị Hạnh Vi <23021455@vnu.edu.vn>
2025-04-25 23:22:28 +0700

    Update bai1.cpp

--- Lect7.0_Assignments_Recursion/bai1.cpp
+++ Lect7.0_Assignments_Recursion/bai1.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int tinhtong(int n){
-	if (n == 1)
-	return 1;
-	else 
-	return n + tinhtong( n - 1);
+// Hàm đệ quy tính tổng các số từ 1 đến n
+int tinhtong(int n) {
+    if (n == 1)
+        return 1;                 // Trường hợp cơ bản: tổng từ 1 đến 1 là 1
+    else
+        return n + tinhtong(n - 1); // Gọi lại chính nó với n - 1 và cộng thêm n
 }
-int main(){
-	int n;
-	cin >>n;
-	cout <<"Tong tu 1 den" << n <<"la: " << tinhtong(n) << endl;
-	return 0;
+
+int main() {
+    int n;
+    cin >> n;
+    // In ra tổng từ 1 đến n bằng cách gọi hàm tinhtong
+    cout << "Tong tu 1 den " << n << " la: " << tinhtong(n) << endl;
+    return 0;
 }
