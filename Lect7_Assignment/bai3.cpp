efe5935079f3b03b225831660cd842d8e56fc5e0
Nguyễn Thị Hạnh Vi <23021455@vnu.edu.vn>
2025-04-25 23:26:16 +0700

    Update bai3.cpp

--- Lect7_Assignment/bai3.cpp
+++ Lect7_Assignment/bai3.cpp
@@ -1,14 +1,16 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
 using namespace std;
 
-int gcd(int x, int y){
-	if (y == 0)
-	return x;
-	return gcd(y, x % y);
+// Hàm tính ước chung lớn nhất (GCD) của hai số x và y bằng thuật toán Euclid
+int gcd(int x, int y) {
+    if (y == 0) // Nếu y = 0 thì ước chung lớn nhất là x
+        return x;
+    return gcd(y, x % y); // Đệ quy gọi lại với x = y và y = x % y
 }
-int main(){
-	int x, y;
-	cin >> x >> y;
-	cout <<gcd(x, y) << endl;
-	return 0;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    cout << gcd(x, y) << endl; 
+    return 0;
 }
