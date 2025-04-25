167bd235d12d0b8446d39ba4c9046dd82a40ac8e
Nguyễn Thị Hạnh Vi <23021455@vnu.edu.vn>
2025-04-25 23:27:16 +0700

    Update bai4.cpp

--- Lect7_Assignment/bai4.cpp
+++ Lect7_Assignment/bai4.cpp
@@ -1,15 +1,18 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
 using namespace std;
 
-int main(){
-	int n;
-	cin >> n;
-	int total = 1 << n;
-	for (int i = 0; i < total; i++){
-		bitset<32> bin(i);
-		string s = bin.to_string().substr(32 - n);
-		cout << s << endl;
-	}
-	return 0;
-}
+int main() {
+    int n;
+    cin >> n; 
 
+    int total = 1 << n; // Tính tổng số dãy nhị phân có n bit, bằng 2^n
+
+    // Duyệt qua tất cả các số từ 0 đến 2^n - 1
+    for (int i = 0; i < total; i++) {
+        bitset<32> bin(i); // Biến i thành số nhị phân 32 bit
+        string s = bin.to_string().substr(32 - n); // Lấy n bit cuối cùng (sử dụng substring để cắt bớt phần thừa)
+        cout << s << endl; 
+    }
+
+    return 0; 
+}
